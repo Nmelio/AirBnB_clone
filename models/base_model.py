4fd535cc512111476504a5a3c62142022ae4d432
prosperemebo <emeboprosper@gmail.com>
2023-08-08 13:48:32 +0100

    Working implementation of BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,22 @@ class BaseModel:
                 else:
                     setattr(self, key, value)
         else:
-            self.id = str(uuid.uuid4())
             nowSnapshot = datetime.now()
+            self.id = str(uuid.uuid4())
             self.created_at = nowSnapshot
             self.updated_at = nowSnapshot
+
+    def __str__(self):
+        s = "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return s
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dict_snapshot = self.__dict__.copy()
+        dict_snapshot['__class__'] = self.__class__.__name__
+        dict_snapshot['created_at'] = self.created_at.isoformat()
+        dict_snapshot['updated_at'] = self.updated_at.isoformat()
+        return dict_snapshot
+        
