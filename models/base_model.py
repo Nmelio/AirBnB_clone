9db8e27c91fbb7280db1861433be27085f5ddc35
prosperemebo <emeboprosper@gmail.com>
2023-08-08 13:30:56 +0100

    Implemented BaseModel Init

--- models/base_model.py
+++ models/base_model.py
@@ -9,4 +9,16 @@ from datetime import datetime
 class BaseModel:
     """Class representing the base model."""
 
-    pass
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            nowSnapshot = datetime.now()
+            self.created_at = nowSnapshot
+            self.updated_at = nowSnapshot
